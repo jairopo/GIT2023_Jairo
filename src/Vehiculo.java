0be5cc565936062f9e9dc2264d32d897bebdfbbf
Jairo Andrades <jairo.po8@gmail.com>
2023-02-03 13:38:11 +0100

    Modificada clase Vehiculo, añadido metodo mostrarMarca

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -6,7 +6,13 @@ public class Vehiculo {
   private String matricula;
   
   public Vehiculo(String marca, String modelo, String matricula) {
-    
+    this.marca = marca;
+    this.modelo = modelo;
+    this.matricula = matricula;
+  }
+  
+  public void mostrarMarca() {
+    System.out.println(marca);
   }
   
   
