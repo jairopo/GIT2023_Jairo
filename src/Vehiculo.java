7ec971bd840052283460491009642d8438187aac
Jairo Andrades <jairo.po8@gmail.com>
2023-02-03 13:20:48 +0100

    Creada clase Vehiculo con constructor

---
+++ src/Vehiculo.java
@@ -0,0 +1,13 @@
+
+public class Vehiculo {
+
+  private String marca;
+  private String modelo;
+  private String matricula;
+  
+  public Vehiculo(String marca, String modelo, String matricula) {
+    
+  }
+  
+  
+}
